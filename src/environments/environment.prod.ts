8e216adb71fc32e438cbcf128ce18433a2fe41ba
nihalrapolu <nihalrapolu@github.com>
2019-03-25 14:01:31 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCiiQdsNbNjwNF0F3TfTk9inVkUAxvXXtg",
+    authDomain: "movies-a5318.firebaseapp.com",
+    databaseURL: "https://movies-a5318.firebaseio.com",
+    projectId: "movies-a5318",
+    storageBucket: "",
+    messagingSenderId: "392321470648"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCiiQdsNbNjwNF0F3TfTk9inVkUAxvXXtg",
+    authDomain: "movies-a5318.firebaseapp.com",
+    databaseURL: "https://movies-a5318.firebaseio.com",
+    projectId: "movies-a5318",
+    storageBucket: "",
+    messagingSenderId: "392321470648"
+  }
+};
